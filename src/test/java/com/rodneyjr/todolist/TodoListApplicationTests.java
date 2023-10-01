77271b01a6a890bfe5a2dd13366c8495cf18bb43
Rodney Junior <rodney.junior37@gmail.com>
2023-10-01 15:45:10 -0400

    implementação de outro banco (h2) pasa os testes

--- src/test/java/com/rodneyjr/todolist/TodoListApplicationTests.java
+++ src/test/java/com/rodneyjr/todolist/TodoListApplicationTests.java
@@ -3,11 +3,12 @@ package com.rodneyjr.todolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.rodneyjr.todolist.entity.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodoListApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
@@ -22,11 +23,11 @@ class TodoListApplicationTests {
 			.expectStatus().isOk()
 			.expectBody()
 			.jsonPath("$").isArray()
-			.jsonPath("%.length()").isEqualTo(1)
-			.jsonPath("%[0].nome()").isEqualTo(todo.getNome())
-			.jsonPath("%[0].descricao()").isEqualTo(todo.getDescricao())
-			.jsonPath("%[0].realizado()").isEqualTo(todo.isRealizado())
-			.jsonPath("%[0].prioridade()").isEqualTo(todo.getPrioridade());
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
 	}
 
 
